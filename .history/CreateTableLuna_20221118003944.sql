fb5eb5bd95430025880b196dd07d5976f7876bf9
WINNE113 <lehuuthang12a2thptcl@gmail.com>
2022-12-02 21:56:20 +0700

    Add New DB

---
+++ .history/CreateTableLuna_20221118003944.sql
@@ -0,0 +1,198 @@
+	
+CREATE DATABASE LUNA_HOTEL;
+	
+
+USE LUNA_HOTEL;
+	
+
+	DROP TABLE IF EXISTS Room;
+	DROP TABLE IF EXISTS RoomType;
+	DROP TABLE IF EXISTS RoomStatus;
+	DROP TABLE IF EXISTS RoomRate;
+	DROP TABLE IF EXISTS Hotel;
+	DROP TABLE IF EXISTS City;
+	DROP TABLE IF EXISTS Booking;
+	DROP TABLE IF EXISTS BookingStatus;
+	DROP TABLE IF EXISTS Employee;
+	DROP TABLE IF EXISTS EmployeeRole;
+	DROP TABLE IF EXISTS Guest;
+	DROP TABLE IF EXISTS Bill;
+	DROP TABLE IF EXISTS Payment;
+	DROP TABLE IF EXISTS PaymentStatus;
+	DROP TABLE IF EXISTS PaymentType;
+	DROP TABLE IF EXISTS HotelEvent;
+	DROP TABLE IF EXISTS Discount;
+	DROP TABLE IF EXISTS HotelService;
+	
+
+	CREATE TABLE RoomType(
+		RoomTypeName nvarchar(50) PRIMARY KEY,
+		RoomPrice DECIMAL(7,2) CHECK (RoomPrice>0),
+		RoomImg text,
+		RoomDesc text,
+	)
+	
+
+	CREATE TABLE RoomStatus(
+		RoomStatusID smallint PRIMARY KEY,
+		RoomStatus nvarchar(20) DEFAULT 'AVAILABLE',
+		RoomStatusDesc text,
+	)
+	
+
+	CREATE TABLE RoomRate(
+		RoomRateID smallint PRIMARY KEY,
+		StarRating float CHECK(StarRating >= 0 AND StarRating <= 5),
+		RoomComment text,
+	)
+	
+
+	CREATE TABLE City(
+		CityID smallint PRIMARY KEY,
+		CityName nvarchar(20),
+		PostalCode int,
+	)
+
+
+	CREATE TABLE HotelEvent(
+		EventID smallint PRIMARY KEY,
+		EventTitle nvarchar(50) NOT NULL,
+		EventImg text,
+		EventDiscount float NOT NULL,
+	)
+
+
+	CREATE TABLE Hotel(
+		HotelCode smallint PRIMARY KEY,
+		CityID smallint FOREIGN KEY REFERENCES City(CityID),
+		HotelAddress text NOT NULL,
+		NumRooms smallint NOT NULL,
+		PhoneHotelNo int NOT NULL,
+		HotelDesc text,
+	)
+	
+	CREATE TABLE HotelService (
+		ServiceId int IDENTITY(1,1) PRIMARY KEY,
+		ServiceName nvarchar (50) NOT NULL,
+		ServiceDesc text,
+		ServicePrice float NOT NULL,
+		HotelCode smallint FOREIGN KEY REFERENCES Hotel(HotelCode),
+);
+
+	CREATE TABLE Room(
+		RoomNo smallint IDENTITY(1,1) PRIMARY KEY,
+		Occupancy smallint NOT NULL CHECK (Occupancy>0),
+		FloorNo smallint NOT NULL,
+		RoomRateID smallint FOREIGN KEY REFERENCES RoomRate(RoomRateID),
+		RoomTypeName nvarchar(50) FOREIGN KEY REFERENCES RoomType(RoomTypeName),
+		HotelCode smallint FOREIGN KEY REFERENCES Hotel(HotelCode),
+		RoomStatusID smallint FOREIGN KEY REFERENCES RoomStatus(RoomStatusID),
+	)
+	
+
+	CREATE TABLE BookingStatus(
+		BookingStatusID smallint PRIMARY KEY,
+		BookingStatus nvarchar(30) DEFAULT 'AVAILABLE',
+		BookingDesc nvarchar(50),
+		BookingActive BIT,
+	)
+	
+
+	CREATE TABLE Guest(
+		GuestID int IDENTITY(1,1) PRIMARY KEY,
+		FirstName nvarchar(30) NOT NULL,
+		LastName nvarchar(30) NOT NULL,
+		DOB date,
+		Gen nvarchar(10),
+		CONSTRAINT CHK_Gen CHECK(Gen = 'Male' or Gen = 'Female'),
+		PhoneGuestNo int NOT NULL,
+		Email varchar(50) NOT NULL,
+		GuestPassword varchar(30) NOT NULL,
+		PassportNo int NOT NULL,
+		GuestAddress text,
+		IsMemberShip BIT,
+	)
+	
+
+	CREATE TABLE Booking(
+		BookingID smallint IDENTITY(1,1)PRIMARY KEY,
+		BookingDate date NOT NULL,
+		CheckInDate  date NOT NULL,
+		CheckOutDate  date NOT NULL,
+		NumPerson smallint NOT NULL,
+		BookingRoomCount int,
+		HotelCode smallint FOREIGN KEY REFERENCES Hotel(HotelCode),
+		GuestID int FOREIGN KEY REFERENCES Guest(GuestID),
+		RoomNo smallint FOREIGN KEY REFERENCES Room(RoomNo),
+		BookingStatusID smallint FOREIGN KEY REFERENCES BookingStatus(BookingStatusID),
+	)
+	
+
+	CREATE TABLE EmployeeRole(
+		RoleID smallint PRIMARY KEY,
+		RoleTitle nvarchar(30) NOT NULL,
+		RoleDesc text,
+	)
+	
+
+	CREATE TABLE Employee(
+		EmployeeID smallint IDENTITY(1,1) PRIMARY KEY,
+		FirstName nvarchar(30) NOT NULL,
+		LastName nvarchar(30) NOT NULL,
+		DOB date,
+		Gender nvarchar(10),
+		CONSTRAINT CHK_Gender Check(Gender = 'Male' or Gender = 'Female'),
+		PhoneNo int NOT NULL,
+		Email varchar(50) NOT NULL,
+		EmployeePassword varchar(30) DEFAULT 1,
+		Salary bigint CHECK (Salary>0),
+		CreatedTime time,
+		UpdatedTime time,
+		HotelCode smallint FOREIGN KEY REFERENCES Hotel(HotelCode),
+		RoleID smallint FOREIGN KEY REFERENCES EmployeeRole(RoleID),
+	)
+	
+
+	CREATE TABLE PaymentStatus(
+		PaymentStatusID smallint PRIMARY KEY,
+		PaymentStatus nvarchar(30),
+		PaymentActive BIT,
+	)
+	
+
+	CREATE TABLE PaymentType(
+		PaymentTypeID smallint PRIMARY KEY,
+		CreditCard varchar(15) NOT NULL,
+		PaymentExpireDate date NOT NULL,
+		ChequeNo int NOT NULL,
+	)
+	
+
+	CREATE TABLE Payment(
+		PaymentID int IDENTITY(1,1) PRIMARY KEY,
+		PaymentDate date,
+		PaymentStatusID smallint FOREIGN KEY REFERENCES PaymentStatus(PaymentStatusID),
+		PaymentTypeID smallint FOREIGN KEY REFERENCES PaymentType(PaymentTypeID),
+	)
+	
+
+	CREATE TABLE Discount(
+		DiscountID INT IDENTITY (1,1) PRIMARY KEY,
+		DiscountName NVARCHAR(100) NOT NULL,
+		DiscountDesc text ,
+		DiscountPer INT NOT NULL,
+	)
+
+
+	CREATE TABLE Bill(
+		InvoiceNo int IDENTITY(1,1) PRIMARY KEY,
+		IfLateCheckOut BIT,
+		Discount DECIMAL(7,2),
+		BookingID smallint FOREIGN KEY REFERENCES Booking(BookingID),
+		GuestID int FOREIGN KEY REFERENCES Guest(GuestID),
+		RoomNo smallint FOREIGN KEY REFERENCES Room(RoomNo),
+		PaymentID int FOREIGN KEY REFERENCES Payment(PaymentID),
+		DiscountID int FOREIGN KEY REFERENCES Discount(DiscountID),
+	)
+			 
+
--- src/java/Controllers/web/FiterByTypeBed.java
+++ src/java/Controllers/web/FiterByTypeBed.java
@@ -32,62 +32,64 @@ public class FiterByTypeBed extends HttpServlet {
             throws ServletException, IOException {
 
         String typeBed = request.getParameter("typebed");
+        if (typeBed.equals("[]")) {
 
-        List<String> listTypeBed = new ArrayList<>();  // store all type bed
+        } else {
+            List<String> listTypeBed = new ArrayList<>();  // store all type bed
 
-        Pattern pp = Pattern.compile("\\w+");           // split string price -> int price
-        Matcher m = pp.matcher(typeBed);
+            Pattern pp = Pattern.compile("\\w+");           // split string price -> int price
+            Matcher m = pp.matcher(typeBed);
 
-        while (m.find()) {
-            listTypeBed.add(m.group());
-        }
+            while (m.find()) {
+                listTypeBed.add(m.group());
+            }
 
-        for (String item : listTypeBed) {
-            System.out.println("item: " + item);
-        }
+            for (String item : listTypeBed) {
+                System.out.println("item: " + item);
+            }
 
-        IRoomService roomService = new RoomService();
+            IRoomService roomService = new RoomService();
 
-        List<RoomDetail> listRoom = null;
+            List<RoomDetail> listRoom = null;
 
-        if (listTypeBed.size() == 1) {
-            listRoom = roomService.listRoomByBedType(listTypeBed.get(0));
-        } else if (listTypeBed.size() == 2) {
-            listRoom = roomService.listRoomByTwoBedType(listTypeBed.get(0), listTypeBed.get(1));
-        } else if (listTypeBed.size() == 3) {
-            listRoom = roomService.listRoomByThreeBedType(listTypeBed.get(0), listTypeBed.get(1), listTypeBed.get(2));
-        }
+            if (listTypeBed.size() == 1) {
+                listRoom = roomService.listRoomByBedType(listTypeBed.get(0));
+            } else if (listTypeBed.size() == 2) {
+                listRoom = roomService.listRoomByTwoBedType(listTypeBed.get(0), listTypeBed.get(1));
+            } else if (listTypeBed.size() == 3) {
+                listRoom = roomService.listRoomByThreeBedType(listTypeBed.get(0), listTypeBed.get(1), listTypeBed.get(2));
+            }
 
-        PrintWriter writer = response.getWriter();
+            PrintWriter writer = response.getWriter();
 
-        for (RoomDetail item : listRoom) {
-            writer.print("<div class=\"result_block\">\n"
-                    + "                                    <div class=\"image_hotel\">\n"
-                    + "                                        <img src=\"" + item.getRoomImg() + "\" alt=\"room hotel\">\n"
-                    + "                                    </div>\n"
-                    + "                                    <div class=\"content_hotel\">\n"
-                    + "                                        <div class=\"main_content\">\n"
-                    + "                                            <div class=\"title_hotel\">\n"
-                    + "                                                <span>" + item.getRoomTypeName() + "</span>\n"
-                    + "                                            </div>\n"
-                    + "                                            <div class=\"address_hotel\">\n"
-                    + "                                                <span>" + item.getHotelAddress() + "</span>\n"
-                    + "                                            </div>\n"
-                    + "                                            <div class=\"price_hotel\">\n"
-                    + "                                                <span>From</span>\n"
-                    + "                                                <h5>VND " + item.getRoomPrice() + "</h5>\n"
-                    + "                                                <span>For Night</span>\n"
-                    + "                                            </div>\n"
-                    + "                                        </div>\n"
-                    + "                                        <div class=\"btn_booking\">\n"
-                    + "                                            <a href=\"checkvalid?rid="+item.getRoomId()+"\">Add To Cart</a>\n"
-                    + "                                            <span></span>\n"
-                    + "                                            <span></span>\n"
-                    + "                                            <span></span>\n"
-                    + "                                        </div>\n"
-                    + "                                    </div>\n"
-                    + "                                </div>");
+            for (RoomDetail item : listRoom) {
+                writer.print("<div class=\"result_block\">\n"
+                        + "                                    <div class=\"image_hotel\">\n"
+                        + "                                        <img src=\"" + item.getRoomImg() + "\" alt=\"room hotel\">\n"
+                        + "                                    </div>\n"
+                        + "                                    <div class=\"content_hotel\">\n"
+                        + "                                        <div class=\"main_content\">\n"
+                        + "                                            <div class=\"title_hotel\">\n"
+                        + "                                                <span>" + item.getRoomTypeName() + "</span>\n"
+                        + "                                            </div>\n"
+                        + "                                            <div class=\"address_hotel\">\n"
+                        + "                                                <span>" + item.getHotelAddress() + "</span>\n"
+                        + "                                            </div>\n"
+                        + "                                            <div class=\"price_hotel\">\n"
+                        + "                                                <span>From</span>\n"
+                        + "                                                <h5>VND " + item.getRoomPrice() + "</h5>\n"
+                        + "                                                <span>For Night</span>\n"
+                        + "                                            </div>\n"
+                        + "                                        </div>\n"
+                        + "                                        <div class=\"btn_booking\">\n"
+                        + "                                            <a href=\"checkvalid?rid=" + item.getRoomId() + "\">Add To Cart</a>\n"
+                        + "                                            <span></span>\n"
+                        + "                                            <span></span>\n"
+                        + "                                            <span></span>\n"
+                        + "                                        </div>\n"
+                        + "                                    </div>\n"
+                        + "                                </div>");
+            }
         }
     }
-
 }
